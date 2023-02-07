40b1a6e67ffb3a68a757db9453a52b6eb20489ad
Sourcery AI <>
2023-02-07 09:39:23 +0000

    'Refactored by Sourcery'

--- pdm_shell.py
+++ pdm_shell.py
@@ -30,31 +30,30 @@ class ShellCommand(BaseCommand):
                 self.output("pdm shell | Invoke-Expression")
             else:
                 raise NotImplementedError(f"Shell {shell} is not supported")
+        elif shell in ("bash", "zsh"):
+            self.output(
+                "export PATH=$(pdm info --packages)/bin:$PATH"
+                " && export PYTHONPATH=$(pdm info --packages)/lib:$PYTHONPATH"
+            )
+        elif shell == "fish":
+            self.output(
+                "set -x PATH $(pdm info --packages)/bin $PATH"
+                " && set -x PYTHONPATH $(pdm info --packages)/lib $PYTHONPATH"
+            )
+        elif shell in ("csh", "tcsh"):
+            self.output(
+                "setenv PATH $(pdm info --packages)/bin:$PATH"
+                " && setenv PYTHONPATH $(pdm info --packages)/lib:$PYTHONPATH"
+            )
+        elif shell == "pwsh":
+            split = ";" if os.name == "nt" else ":"
+            bin_dir = "Scripts" if os.name == "nt" else "bin"
+            self.output(
+                f"Set-Item -Path Env:Path -Value ((Join-Path $(pdm info --packages) '{bin_dir}') + '{split}' + $env:PATH)"
+                f" && Set-Item -Path Env:PYTHONPATH -Value ((Join-Path $(pdm info --packages) 'lib') + '{split}' + $env:PYTHONPATH)"
+            )
         else:
-            if shell in ("bash", "zsh"):
-                self.output(
-                    "export PATH=$(pdm info --packages)/bin:$PATH"
-                    " && export PYTHONPATH=$(pdm info --packages)/lib:$PYTHONPATH"
-                )
-            elif shell == "fish":
-                self.output(
-                    "set -x PATH $(pdm info --packages)/bin $PATH"
-                    " && set -x PYTHONPATH $(pdm info --packages)/lib $PYTHONPATH"
-                )
-            elif shell in ("csh", "tcsh"):
-                self.output(
-                    "setenv PATH $(pdm info --packages)/bin:$PATH"
-                    " && setenv PYTHONPATH $(pdm info --packages)/lib:$PYTHONPATH"
-                )
-            elif shell == "pwsh":
-                split = ";" if os.name == "nt" else ":"
-                bin_dir = "Scripts" if os.name == "nt" else "bin"
-                self.output(
-                    f"Set-Item -Path Env:Path -Value ((Join-Path $(pdm info --packages) '{bin_dir}') + '{split}' + $env:PATH)"
-                    f" && Set-Item -Path Env:PYTHONPATH -Value ((Join-Path $(pdm info --packages) 'lib') + '{split}' + $env:PYTHONPATH)"
-                )
-            else:
-                raise NotImplementedError(f"Shell {shell} is not supported")
+            raise NotImplementedError(f"Shell {shell} is not supported")
 
     def output(self, command: str) -> None:
         print(command, flush=True)
